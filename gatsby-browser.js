f2d8930efb13147c95ab429248fbd186b00557ba
David Corwin <david.corwin@gsa.gov>
2019-11-25 09:58:31 -0800

    Configured DAP

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,22 +6,23 @@
 import "./src/styles/index.scss"
 import "uswds"
 
+import { siteMetadata } from "./gatsby-config"
+
+let loaded = false
+
 const dap = pathname => {
   window.gas && window.gas("send", "pageview", pathname)
 }
 
-let loaded = false
-const onload = () => {
-  dap(window.location.pathname)
-  loaded = true
-}
-
-export const onInitialClientRender = (_, opts) => {
+export const onInitialClientRender = () => {
   const script = document.createElement("script")
-  script.src =
-    "https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency={{site.dap_agency}}"
+  script.src = `https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=${siteMetadata.dapAgency}`
   script.id = "_fed_an_ua_tag"
-  script.onload = onload
+  script.onload = () => {
+    dap(window.location.pathname)
+    loaded = true
+  }
+
   document.body.appendChild(script)
 }
 
